9c3b82e5d44dcff81e068dfca4b8128316e8d6f9
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-11-30 10:44:36 -0200

    Webservice initial code

---
+++ MyTubeWS/src/java/com/mytubews/bean/FileBean.java
@@ -0,0 +1,34 @@
+package com.mytubews.bean;
+
+import javax.activation.DataHandler;
+
+public class FileBean {
+	private String name;
+	private String description;
+	private DataHandler data;
+
+	public DataHandler getData() {
+		return data;
+	}
+
+	public void setData(DataHandler data) {
+		this.data = data;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
